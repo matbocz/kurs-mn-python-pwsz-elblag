00cc3025d7252ade41d2836566676cc4140aa002
Mateusz Boczarski <matbocz@outlook.com>
2018-10-13 13:56:18 +0200

    Create lab2/zad1.py

---
+++ lab2/zad1.py
@@ -0,0 +1,8 @@
+#ZADANIE 1
+#Wygeneruj automatycznie nastepujace pary liczb:
+#[(2, 11), (2, 17), (2, 5), (5, 11), (5, 17), (17, 11), (17, 5)]
+#Zauwaz, że wynikiem jest lista zawierajaca pary liczb zapisane w krotkach.
+#Napisz kod zgodnie z wytycznymi z wykladu.
+
+s1=[(x,y) for x in [2,5,17] for y in [11,17,5] if x!=y]
+print(s1)
