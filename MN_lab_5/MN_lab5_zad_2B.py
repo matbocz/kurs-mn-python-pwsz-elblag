9cd894e30cd3d0ddd333afd7f31d8a17f6c2b62b
Mateusz Boczarski <matbocz@outlook.com>
2018-11-13 20:01:09 +0100

    Create MN_lab_5\MN_lab5_zad_2B.py

---
+++ MN_lab_5/MN_lab5_zad_2B.py
@@ -0,0 +1,24 @@
+#Zadanie 2
+#Napisz program realizujacy interpolacje za pomoca funkcji numpy.polyfit i numpy.polyval dla zbioru danych wejsciowych.
+#Przyjmij, że zbior ten jest dwuwymiarowy.
+#Pokaż testbench implementacji dla zbiorow danych pierwotnych wygenerowanych wg funkcji:
+#sin(1/x), exp(cos(x)).
+#Zbiory danych wejsciowych generuj z krokiem 0.4 w zakresie -pi do pi.
+#Wartosci 'x' dla danych interpolowanych generuj „gesto”, np. z krokiem 0.08.
+#Przedstaw graficzne interpretacje.
+
+#Przyklad exp(cos(x)).
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = [i for i in np.arange(-np.pi, np.pi, 0.4)]
+y = [np.exp(np.cos(i)) for i in np.arange(-np.pi, np.pi, 0.4)]
+
+wielomian = np.polyfit(x, y, 6)
+
+xinterp = np.arange(-np.pi, 2.8, 0.08)
+yinterp = np.polyval(wielomian, xinterp)
+
+plt.plot(x, y, 'or', xinterp, yinterp, '.b')
+plt.show()
